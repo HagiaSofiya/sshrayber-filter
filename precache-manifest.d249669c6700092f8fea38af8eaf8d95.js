13d1b0909df0a142c932cc9ebff21e642f6abb35
HagiaSofiya <SofiyaShrayber@gmail.com>
2019-03-04 12:29:43 -0500

    Updates

---
+++ precache-manifest.d249669c6700092f8fea38af8eaf8d95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15073dbf27d7b4a53393",
+    "url": "/sshrayber-filter/static/css/main.64d20950.chunk.css"
+  },
+  {
+    "revision": "15073dbf27d7b4a53393",
+    "url": "/sshrayber-filter/static/js/main.15073dbf.chunk.js"
+  },
+  {
+    "revision": "44c0ef6ac009d605b6fd",
+    "url": "/sshrayber-filter/static/js/runtime~main.44c0ef6a.js"
+  },
+  {
+    "revision": "b8e7742f4c7369bdcd73",
+    "url": "/sshrayber-filter/static/js/2.b8e7742f.chunk.js"
+  },
+  {
+    "revision": "d4eb5f71b2d2a4b02bef297749cc17dc",
+    "url": "/sshrayber-filter/index.html"
+  }
+];
--- src/styles/AppStyle.js
+++
@@ -1,7 +0,0 @@
-const mainStyle = {
-    "color": "#373633",
-    "margin": "0 auto",
-    "width": "80vw"
-}
-
-export { mainStyle } 
