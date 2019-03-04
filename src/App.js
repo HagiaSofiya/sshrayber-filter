52c280c23610e19d51305cb00bec6e965e3f78f4
HagiaSofiya <SofiyaShrayber@gmail.com>
2019-03-03 20:37:00 -0500

    [js] Filter functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Filter from './Filter';
 import './App.css';
 
 
@@ -6,7 +7,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-
+				<Filter/>
 			</div>
 		);
 	}
---
+++ src/Filter.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import screens from './data.json';
+
+
+class Filter extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            screens: screens,
+            filteredScreens: screens,
+        }
+    }
+
+    compareBy(key){
+        this.setState({
+            filteredScreens: this.state.screens.filter(screen => {
+                return screen.name === key ;
+            })
+        })
+    }
+
+    reset(){
+        this.setState({
+            filteredScreens: this.state.screens
+        })
+    }
+
+    render(){
+        const { filteredScreens, screens } = this.state
+        const categories = [...new Set(screens.map(screen => screen.name))]
+        
+        return (
+            <div>
+                {categories.map(category => {
+					return (
+                            <button 
+                                type="button" 
+                                onClick={() => this.compareBy(category)} key={category}>
+                                {category}
+                            </button>
+                        )
+				})}
+                <button 
+					type="button" 
+					onClick={() => this.reset()}>
+                    all
+                </button>
+                {filteredScreens.map((screens) => {
+                    return (
+                        <figure key={screens.key}>
+                            <img src={screens.url} alt={screens.name}/>
+                            <figcaption>{screens.name}</figcaption>
+                        </figure>
+
+                    )
+                })}                
+            </div>
+        )
+    }
+}
+
+export default Filter;
