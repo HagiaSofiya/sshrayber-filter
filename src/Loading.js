ab7efabfb233fe85d0694e897ef623bc13a29080
HagiaSofiya <SofiyaShrayber@gmail.com>
2019-03-04 19:51:12 -0500

    [js] Loading js

---
+++ src/Loading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { LoadingStyle, LoadingSpinner} from './styles/LoadingStyle'
+
+const Loading = () => (
+	<div style={LoadingStyle}>
+		<svg
+			style={LoadingSpinner}
+			className='loading-spinner'
+			width="60"
+			height="20"
+			viewBox="0 0 60 20"
+			xmlns="http://www.w3.org/2000/svg"
+			>
+			<circle cx="7" cy="15" r="4"/>
+			<circle cx="30" cy="15" r="4"/>
+			<circle cx="53" cy="15" r="4"/>
+		</svg>
+	</div>
+)
+  
+export default Loading;
