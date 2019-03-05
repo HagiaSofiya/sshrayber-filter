2a3e9c5d1a8cfc791bb0fd33f8105d447422a2bb
HagiaSofiya <SofiyaShrayber@gmail.com>
2019-03-04 19:45:02 -0500

    [style] Loading animation

---
+++ src/styles/LoadingStyle.js
@@ -0,0 +1,12 @@
+const LoadingStyle = {
+	"padding": "50px 0",
+    "width": "100%"
+}
+
+const LoadingSpinner = {
+    "display": "block",
+    "fill": "#4dd2c2",
+	"width": "100%"
+}
+
+export { LoadingStyle , LoadingSpinner }
